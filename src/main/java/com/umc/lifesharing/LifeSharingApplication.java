2816201228449df6e0a7efe8e5a839391c7188ae
jinuk <cimaron713@gmail.com>
2024-01-07 11:02:04 +0900

    initial

---
+++ src/main/java/com/umc/lifesharing/LifeSharingApplication.java
@@ -0,0 +1,13 @@
+package com.umc.lifesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeSharingApplication.class, args);
+    }
+
+}
