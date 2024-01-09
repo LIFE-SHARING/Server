ede25ece8e67039498d948aaf4370cac4cd4622b
bbangHo <sonsins1@naver.com>
2024-01-09 23:18:31 +0900

    feat: 스프링 시큐리티 설정, 회원가입 구현 중

--- src/main/java/com/umc/lifesharing/user/dto/UserRequestDTO.java
+++ src/main/java/com/umc/lifesharing/user/dto/UserRequestDTO.java
@@ -2,12 +2,14 @@ package com.umc.lifesharing.user.dto;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 public class UserRequestDTO {
 
@@ -16,18 +18,14 @@ public class UserRequestDTO {
     @AllArgsConstructor
     @Getter
     public static class JoinDTO {
+        @NotEmpty(message = "이메일은 필수 입력 값입니다.")
         @Email(message = "이메일 형식이 아닙니다.")
         private String email;
 
-        @Size(min = 8, max = 16)
+        @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+        @Length(min = 8, max = 16)
         @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
         private String password;
-
-        @Size(min = 8, max = 16)
-        @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
-        private String vailedPassword;
-
-
     }
 
 
--- src/main/java/com/umc/lifesharing/user/repository/UserRepository.java
+++ src/main/java/com/umc/lifesharing/user/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.umc.lifesharing.user.repository;
 
-public interface UserRepository {
+import com.umc.lifesharing.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+    public User findByEmail(String email);
 }
