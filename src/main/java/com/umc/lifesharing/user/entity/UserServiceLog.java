291939bc6087887f0d0770d31ec58b6a621ec7c3
bbangHo <sonsins1@naver.com>
2024-01-10 23:01:32 +0900

    feat: 엔티티 Lazy 전략 추가 #2

--- src/main/java/com/umc/lifesharing/user/entity/UserServiceLog.java
+++ src/main/java/com/umc/lifesharing/user/entity/UserServiceLog.java
@@ -12,7 +12,7 @@ public class UserServiceLog {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
