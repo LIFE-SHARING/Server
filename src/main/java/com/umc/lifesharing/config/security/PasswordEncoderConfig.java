3ef62fe46bb7dc94cb2df695c6f02cb640e1afa0
bbangHo <sonsins1@naver.com>
2024-01-14 21:00:25 +0900

    feat: security config 순환 참조 문제 해결 #2

--- src/main/java/com/umc/lifesharing/config/security/PasswordEncoderConfig.java
+++ src/main/java/com/umc/lifesharing/config/security/PasswordEncoderConfig.java
@@ -2,6 +2,7 @@ package com.umc.lifesharing.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,6 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordEncoderConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
