c93136cd119889d5773b13010f70404da0db65e6
jinuk <cimaron713@gmail.com>
2024-01-15 13:55:38 +0900

    feat: 웹소켓 관련 임시 파일 생성 #10

---
+++ src/main/java/com/umc/lifesharing/chat/temp/TempChatDTO.java
@@ -0,0 +1,19 @@
+//package com.umc.lifesharing.chat.temp;
+//
+//import lombok.Builder;
+//import lombok.Data;
+//
+//@Data
+//@Builder
+//public class TempChatDTO {
+//    // 메시지  타입 : 입장, 채팅
+//    public enum MessageType{
+//        ENTER, TALK
+//    }
+//
+//    private MessageType type; // 메시지 타입
+//    private String roomId; // 방 번호
+//    private String sender; // 채팅을 보낸 사람
+//    private String message; // 메시지
+//    private String time; // 채팅 발송 시간간
+//}
