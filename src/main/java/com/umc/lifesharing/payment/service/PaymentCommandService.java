25f844ffa62ae205c9926a8d665ca12d9b6aec8b
yun0529 <0529yun@naver.com>
2024-01-15 17:19:30 +0900

    toss payments 테스트 결제 성공 #7

--- src/main/java/com/umc/lifesharing/payment/service/PaymentCommandService.java
+++ src/main/java/com/umc/lifesharing/payment/service/PaymentCommandService.java
@@ -3,9 +3,14 @@ package com.umc.lifesharing.payment.service;
 
 import com.umc.lifesharing.payment.dto.TossPaymentSuccessDto;
 import com.umc.lifesharing.payment.entity.TossPayment;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.ProtocolException;
 
 public interface PaymentCommandService {
     public TossPayment requestTossPayment(TossPayment payment, Long userId);
-    public TossPaymentSuccessDto tossPaymentSuccess(String paymentKey, String orderId, Long amount);
-    public TossPaymentSuccessDto requestPaymentAccept(String paymentKey, String orderId, Long amount);
+    public TossPaymentSuccessDto tossPaymentSuccess(String paymentKey, String orderId, Long amount) throws IOException, ParseException;
+    public TossPaymentSuccessDto requestPaymentAccept(String paymentKey, String orderId, Long amount) throws IOException, ParseException;
 }
