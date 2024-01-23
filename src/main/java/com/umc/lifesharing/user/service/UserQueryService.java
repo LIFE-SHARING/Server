9a5c80f5832d804e3f6fb0026283fbb8d6fff788
bbangHo <sonsins1@naver.com>
2024-01-23 20:07:50 +0900

    feat: 회원가입 이메일 중복 검증 API 구현 #1

--- src/main/java/com/umc/lifesharing/user/service/UserQueryService.java
+++ src/main/java/com/umc/lifesharing/user/service/UserQueryService.java
@@ -1,6 +1,7 @@
 package com.umc.lifesharing.user.service;
 
 import com.umc.lifesharing.product.entity.Product;
+import com.umc.lifesharing.user.dto.UserRequestDTO;
 import com.umc.lifesharing.user.entity.User;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,10 @@ public interface UserQueryService extends UserDetailsService {
     UserResponseDTO.MyPageResponseDTO getMyPage(UserAdapter userAdapter);
     UserResponseDTO.UserInfoResponseDTO getUserInfo(UserAdapter userAdapter);
     Optional<User> findByUser(Long id);
+
+//    Boolean checkEmail(String email);
+
+    UserResponseDTO.CheckNicknameResponseDTO existNickname(UserRequestDTO.CheckNickname checkNickname);
 
     // 회원의 제품 리스트
     List<Product> getProductList(Long memberId);
